24621e8f443e41e1043da5c8120b7cbf533bbf02
Trisha Erlapalli <ttrishaerlapalli@gmail.com>
2025-02-16 20:57:05 +0530

    Add files via upload

---
+++ saikettask1/script.js
@@ -0,0 +1,5 @@
+document.getElementById("readMore").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Redirecting to the full article...");
+    window.location.href = "full-article.html"; // Replace with actual URL
+});
